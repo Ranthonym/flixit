b3ef5ffcad991395fe360b5856690dd77a99d711
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-09-30 20:20:02 -0400

    create featured content component with css styling

---
+++ client/src/components/Featured/Featured.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Featured.scss";
+
+import profile from "../../images/profile.jpeg";
+import { InfoOutlined, PlayArrow } from "@material-ui/icons";
+
+const Featured = ({ type }) => {
+  return (
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
+      <img src={profile} alt="featured" />
+      <div className="info">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/2/20/NormMacdonaldHasAShow.png"
+          alt="featured"
+        />
+        <span className="desc">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import "./home.scss";
 
-import profile from "../images/profile.jpeg";
+import Featured from "../components/Featured/Featured";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img width="100%" src={profile} alt="profile" />
+
+      <Featured type="movie" />
     </div>
   );
 };
