94ad0ebcbd388764389da51932deab67b71e11ba
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-10-08 00:18:18 -0400

    create server controller functions for user CRUD operations

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 //Register
@@ -16,6 +17,34 @@ router.post("/register", async (req, res) => {
   try {
     const user = await new User.Save();
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json({ message: "User not found" });
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong username or password");
+
+    // create web token
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+
+    // separate encrypted password from user data.
+    const [password, ...userData] = user._doc;
+
+    res.status(200).json({ ...userData, accessToken });
   } catch (error) {
     res.status(500).json(error);
   }
